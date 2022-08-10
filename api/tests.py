53a0a5eddfa9a7e4529040f2476e8029c0366e3a
許哲瑋 <wayne@xuzheweideMacBook-Air.local>
2022-08-10 17:01:04 +0800

    EDIT: api/tests.py

--- api/tests.py
+++ api/tests.py
@@ -1,8 +1,39 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from django.conf import settings
 
 
 # Create your tests here.
 class OrderTestCase(APITestCase):
-    # Add your testcase here
-    pass
+    '''Test for post new order'''
+    def setUp(self):
+        #set url for post order
+        self.post_url = reverse('post-order')
+        #set userA with authorization token
+        self.client_a = APIClient()
+        self.client_a.credentials(HTTP_AUTHORIZATION=settings.ACCEPTED_TOKEN)    
+        #set userB with authorization token 
+        self.client_b = APIClient()
+
+    def test_post_order_with_token(self):
+        '''with authentication token'''
+        data = {'total_price':999}
+        response = self.client_a.post(self.post_url,data,format='json')
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+
+
+    def test_post_order_without_token(self):
+        '''without authentication token'''
+        data = {'total_price':888}
+        response = self.client_b.post(self.post_url,data,format='json')
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+
+
+    def test_post_order_with_invalid_data(self):
+        '''use invalid data'''
+        data = {'total_price':"not a anumber"}
+        response = self.client_a.post(self.post_url,data,format='json')
+        self.assertEqual(response.status_code,status.HTTP_400_BAD_REQUEST)   
--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from api.views import import_order, list_order, specific_order
 urlpatterns = [
     path('list-order/', list_order ),
     path('list-order/<int:id>', specific_order),
-    path('import-order/', import_order)
+    path('import-order/', import_order, name='post-order')
 ]
