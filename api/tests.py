77c19c94b848ae132e04da551a24c346c30724f5
uuuChen <fredusr1@gmail.com>
2023-07-03 23:49:58 +0800

    feat: support basic requirements

--- api/tests.py
+++ api/tests.py
@@ -1,8 +1,17 @@
 from django.test import TestCase
 from rest_framework.test import APITestCase
+from django.urls import reverse
+from .models import Order
 
-
-# Create your tests here.
 class OrderTestCase(APITestCase):
-    # Add your testcase here
-    pass
+    def test_import_order(self):
+        url = reverse('import_order')
+        data = {
+            'order_number': '12345',
+            'total_price': 100.00,
+            'created_time': '2023-07-01T00:00:00Z',
+        }
+        response = self.client.post(url, data, HTTP_AUTHORIZATION='omni_pretest_token')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Order.objects.count(), 1)
+        self.assertEqual(Order.objects.get().order_number, '12345')
--- api/views.py
+++ api/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponseBadRequest
+from django.http import HttpResponseBadRequest, HttpResponse
 from rest_framework.decorators import api_view
+from .models import Order
+import json
 # Create your views here.
 
 
 ACCEPTED_TOKEN = ('omni_pretest_token')
 
-
 @api_view(['POST'])
 def import_order(request):
-    # Add your code here
-    return HttpResponseBadRequest()
+    token = request.headers.get('Authorization')
+    if token != ACCEPTED_TOKEN:
+        return HttpResponseBadRequest()
+
+    data = json.loads(request.body)
+    Order.objects.create(
+        order_number=data['order_number'],
+        total_price=data['total_price'],
+        created_time=data['created_time'],
+    )
+
+    return HttpResponse(status=200)
+
+
+def home(request):
+    return HttpResponse("Welcome to my Django app!")
