0f6e72814aee6584195ebb066e66aef0f7c9b72e
Pin-Ching Chen <p.ching0811@gmail.com>
2024-08-17 18:49:19 +0800

    feat(api): add Order model with order_number, total_price, and created_time

--- api/models.py
+++ api/models.py
@@ -4,5 +4,7 @@ from django.db import models
 # Create your models here.
 class Order(models.Model):
     # Add your model here
-    pass
+    order_number = models.CharField(max_length=63, unique=True)
+    total_price  = models.PositiveIntegerField()
+    created_time = models.DateTimeField(auto_now_add=True)
 
