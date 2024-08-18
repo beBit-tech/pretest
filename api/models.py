6d58895ef007f0e52048959d83a323a2b01c2a39
Pin-Ching Chen <p.ching0811@gmail.com>
2024-08-18 15:39:33 +0800

    feat(api): construct Product model and establish many-to-many relationship between Order and Product with OrderProduct

--- api/models.py
+++ api/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
-
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=0)
+
+
 class Order(models.Model):
     order_number = models.CharField(max_length=63, unique=True)
-    total_price  = models.PositiveIntegerField()
+    total_price = models.PositiveIntegerField()
     created_time = models.DateTimeField(auto_now_add=True)
 
+    # Relationship with Product
+    products = models.ManyToManyField(
+        Product, through="OrderProduct", related_name="orders"
+    )
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
