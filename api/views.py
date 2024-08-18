46a224faccd74879996eabb11d2d79f1edb390e3
Pin-Ching Chen <p.ching0811@gmail.com>
2024-08-18 14:36:10 +0800

    refactor(api): replace token validation logic with a decorator

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 from django.http import HttpResponseBadRequest
 from django.shortcuts import render
 from rest_framework import status
@@ -10,20 +12,38 @@ from .serializers import OrderSerializer
 # Create your views here.
 
 
-ACCEPTED_TOKEN = ('omni_pretest_token')
+ACCEPTED_TOKEN = "omni_pretest_token"
 
+
+# Original function to check api aceess token
 def validate_token(token):
     return token == ACCEPTED_TOKEN
 
 
-@api_view(['POST'])
+def check_token(func):
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        token = request.headers.get("Authorization")
+        if token != ACCEPTED_TOKEN:
+            return Response(
+                {"error": "Unauthorized"}, status=status.HTTP_401_UNAUTHORIZED
+            )
+        return func(request, *args, **kwargs)
+
+    return wrapper
+
+
+@api_view(["POST"])
+@check_token
 def import_order(request):
+    # The original token validation code has been replaced by the @require_token decorator.
+
     # Extract the token from the request headers
-    token = request.headers.get('Authorization')
+    # token = request.headers.get('Authorization')
 
     # Validate the extracted token
-    if not validate_token(token):
-        return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+    # if not validate_token(token):
+    # return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
 
     # Parse the data from request using serializer
     serializer = OrderSerializer(data=request.data)
@@ -32,10 +52,10 @@ def import_order(request):
     if serializer.is_valid():
         # save the valid data
         order = serializer.save()
-        return Response({
-            'order_id': order.id,
-            'order': OrderSerializer(order).data
-        }, status=status.HTTP_201_CREATED)
+        return Response(
+            {"order_id": order.id, "order": OrderSerializer(order).data},
+            status=status.HTTP_201_CREATED,
+        )
 
     # If data is invalid, return a 400 response
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
