5ee3117e542530822ad76771577bf3e5ef48cdbe
Pin-Ching Chen <p.ching0811@gmail.com>
2024-08-18 10:48:43 +0800

    feat(api): implement import_order API with token validation

--- api/views.py
+++ api/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
 from django.http import HttpResponseBadRequest
+from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Order
+from .serializers import OrderSerializer
+
 # Create your views here.
 
 
 ACCEPTED_TOKEN = ('omni_pretest_token')
 
+def validate_token(token):
+    return token == ACCEPTED_TOKEN
+
 
 @api_view(['POST'])
 def import_order(request):
-    # Add your code here
-    return HttpResponseBadRequest()
+    # Extract the token from the request headers
+    token = request.headers.get('Authorization')
+
+    # Validate the extracted token
+    if not validate_token(token):
+        return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    # Parse the data from request using serializer
+    serializer = OrderSerializer(data=request.data)
+
+    # Validate the data
+    if serializer.is_valid():
+        # save the valid data
+        order = serializer.save()
+        return Response({
+            'order_id': order.id,
+            'order': OrderSerializer(order).data
+        }, status=status.HTTP_201_CREATED)
+
+    # If data is invalid, return a 400 response
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
