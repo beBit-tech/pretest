64b5eb0a59a74dcd7296a597c274587ebfbe4bef
needful0612 <106731646+needful0612@users.noreply.github.com>
2024-08-26 20:09:41 +0800

    feat(api): implement product model and build relationship with order

--- api/models.py
+++ api/models.py
@@ -2,8 +2,22 @@ from django.db import models
 
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField()
+    amount = models.PositiveIntegerField(default = 0) #amount on stock
+
+
 class Order(models.Model):
     order_number = models.CharField(max_length=255, unique=True)
     total_price = models.PositiveIntegerField()
     created_time = models.DateTimeField(auto_now_add=True)
+    products = models.ManyToManyField(Product, through="ProductOrder")
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default = 1)
+
 
