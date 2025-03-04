cbe7f0df3d1f006577084ffac39dc28fad66f15c
valkyraycho <razor0424@gmail.com>
2025-03-04 06:09:57 -0800

    contruct order model

--- api/models.py
+++ api/models.py
@@ -4,5 +4,9 @@ from django.db import models
 # Create your models here.
 class Order(models.Model):
     # Add your model here
-    pass
+    order_number = models.IntegerField(unique=True)
+    total_price = models.DecimalField(decimal_places=2, max_digits=10)
+    created_time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self) -> str:
+        return f"Order #{self.order_number} - ${self.total_price} ({self.created_time.strftime('%Y-%m-%d %H:%M')})"
