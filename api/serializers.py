c66646fa6e1566588225e8c71073ecb07f1b255e
valkyraycho <razor0424@gmail.com>
2025-03-05 10:01:30 -0800

    add product serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Order
+from .models import Order, Product
 
 
 class OrderSerializer(serializers.ModelSerializer):
@@ -8,3 +8,15 @@ class OrderSerializer(serializers.ModelSerializer):
         model = Order
         fields = ("order_number", "total_price", "created_time")
         read_only_fields = ("created_time",)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            "id",
+            "name",
+            "description",
+            "price",
+            "created_time",
+        )
