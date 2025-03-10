d0e8a5d74e7f01d140a7fa41f9f6ffec71cbae4c
Jun-Wei Liu <1026oscar1@gmail.com>
2025-03-10 14:25:07 +0800

    Change order_number and product_id to UUIDField, set as primary key

--- api/models.py
+++ api/models.py
@@ -3,28 +3,27 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    product_id = models.CharField(max_length=100, unique=True)
+    product_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.name
-    
-    @classmethod
-    def generate_product_id(cls):
-        last_product = cls.objects.last()
-        if last_product:
-            return str(int(last_product.product_id) + 1)
-        return '1'
+        return self.product_id + ' - ' + self.name
     
     @classmethod
     def create_product(cls, name, price):
-        product_id = cls.generate_product_id()
-        return cls.objects.create(product_id=product_id, name=name, price=price)
+        """
+        Create a product with the given name and price.
+        
+        :param name: The name of the product
+        :param price: The price of the product
+        :return: The created product
+        """
+        return cls.objects.create(name=name, price=price)
 
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=100, unique=True)
+    order_number = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_time = models.DateTimeField(auto_now_add=True)
     products = models.ManyToManyField(Product, related_name='orders')
@@ -32,13 +31,6 @@ class Order(models.Model):
     def __str__(self):
         return self.order_number
 
-    @classmethod
-    def generate_order_number(cls):
-        last_order = cls.objects.last()
-        if last_order:
-            return str(int(last_order.order_number) + 1)
-        return '1'
-    
     @classmethod
     def create_order(cls, total_price, product_ids):
         order_number = cls.generate_order_number()
