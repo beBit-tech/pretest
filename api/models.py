e0db6c202c0e33355167fb5f25b6eb69b072f7a5
William <williamyeh@billows.com.tw>
2025-04-10 15:21:23 +0800

    feat: add order model

--- api/models.py
+++ api/models.py
@@ -4,5 +4,10 @@ from django.db import models
 # Create your models here.
 class Order(models.Model):
     # Add your model here
-    pass
+    order_number = models.CharField(max_length=100, unique=True) # 訂單編號
+    total_price = models.DecimalField(max_digits=10, decimal_places=2) # 訂單總價
+    created_time = models.DateTimeField(auto_now_add=True) # 訂單建立時間
+
+    def __str__(self):
+        return f"Order {self.order_number}"
 
