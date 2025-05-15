51c81bfe447c1308353829db7575f4d1f2016dd0
minshuan <0917wasd@gmail.com>
2025-05-15 19:02:24 +0800

    Add API unittest and update views

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from api.views import import_order, list_orders
 
 urlpatterns = [
-    path('import_order/', import_order),
-    path('orders/', list_orders),
+    path('import_order/', import_order, name='import_order'),
+    path('orders/', list_orders, name='list_orders'),
 ]
